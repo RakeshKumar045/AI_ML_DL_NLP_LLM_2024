4f93fe7a8c8b60a9664840d70d7294885389ea3a
Nikky06184 <nikky06184@gmail.com>
2025-05-26 01:46:10 +0530

    windows2

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def print_hi(name):
 
 print("12345 windows1")
 
+print("12345 windows2")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm test45333')
